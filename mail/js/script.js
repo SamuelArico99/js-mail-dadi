af88008a24de13bb56dd2e20d1ef8e8dffc3d172
SamuelArico99 <samuel16699.sa@gmail.com>
2022-12-22 18:12:13 +0100

    fine mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,17 +2,15 @@
 const email = prompt('Inserisci la tua mail , vediamo se sei presente nella lista')
 const emailList = ["paolo@mail.com","luca@mail.com","giovanni@mail.com","matteo@mail.com","pino@mail.com"]
 // controlla che sia nella lista di chi può accedere
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < emailList.length; i++) {
     if (email == emailList[i]) {
         // stampa un messaggio appropriato sull'esito del controllo.
         alert('Si, la tua mail è presente nella lista');
+        break;
+    }
+    else if (i == emailList.length -1) {
+        alert('No , non sei nella lista');
     }
        
 }
-for (let i = 0; i < 5; i++) {
-    if (email != emailList[i]) {
-        // stampa un messaggio appropriato sull'esito del controllo.
-        alert('No, la tua mail non è presente nella lista');
-    }
-       
-}
+
